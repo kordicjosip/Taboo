21c81a277b45f7fe0a03fb35bda2efcc03dc4383
Matej Miljko <matej@vanima.com>
2021-06-22 16:15:59 +0200

    izmjene url-a apija

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {NgxLoggerLevel} from "ngx-logger";
 
 export const environment = {
   production: true,
-  apiURL: 'https://dalekaobala.ba/api/',
-  wsURL: 'wss://dalekaobala.ba/api/ws/',
+  apiURL: 'https://api.dalekaobala.ba/api/',
+  wsURL: 'wss://api.dalekaobala.ba/api/ws/',
   logLevel: NgxLoggerLevel.OFF
 };
