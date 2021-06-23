48142a947a4f3992f2fcaf1fe385e8a6701df143
Matej Miljko <matej@vanima.com>
2021-06-23 19:06:36 +0200

    Osnova prikaza stolova

---
+++ src/app/home/tables/tables.component.ts
@@ -0,0 +1,82 @@
+import {Component, OnInit} from '@angular/core';
+import * as d3 from 'd3';
+import {NGXLogger} from "ngx-logger";
+
+@Component({
+  selector: 'app-tables',
+  templateUrl: './tables.component.html',
+  styleUrls: ['./tables.component.sass']
+})
+export class TablesComponent implements OnInit {
+  private g: any;
+
+  constructor(
+    private logger: NGXLogger
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.logger.debug("Initializing tables component");
+
+    const svg = d3.select("div#tables")
+      .append("svg")
+      .attr("viewBox", "0 0 1200 800");
+
+    const fixedLayout = svg.append("g");
+
+    fixedLayout.append('circle')
+      .attr('cx', 20)
+      .attr('cy', 20)
+      .attr('r', 15)
+      .attr('fill', '#000')
+      .on('mouseover', function (d, i) {
+        d3.select(this).transition()
+          .duration(500)
+          .attr('opacity', '.1');
+        console.log(d)
+      })
+      .on('mouseout', function (d, i) {
+        d3.select(this).transition()
+          .duration(500)
+          .attr('opacity', '1');
+        console.log(d)
+      })
+    fixedLayout.append('rect')
+      .attr('fill', 'rgba(0,0,0,0.01)')
+      .attr('x', 0)
+      .attr("y", 0)
+      .attr("width", 1200)
+      .attr("height", 800)
+      .attr('stroke', '#2378ae')
+      .attr('stroke-dasharray', '10,5')
+      .attr('stroke-linecap', 'butt')
+      .attr('stroke-width', '3');
+
+
+    const g = svg.append("g");
+    this.g = g;
+
+    svg.call(d3.zoom<any, any>()
+      .extent([[0, 0], [1200, 800]])
+      .scaleExtent([0.25, 4])
+      .on("zoom", function (event: any) {
+        g.attr("transform", event.transform)
+      }));
+
+    this.logger.debug("Initialized tables");
+  }
+
+  append() {
+    this.logger.debug("Appending circle")
+    const g = this.g.append("g");
+    const circle = g.append('circle')
+      .attr('cx', Math.floor(Math.random() * 1200))
+      .attr('cy', Math.floor(Math.random() * 800))
+      .attr('r', 50)
+      .attr('fill', '#69a3b2');
+    g.append('text')
+      .text('Text')
+      .attr('x', circle.node().getBBox().x + 35)
+      .attr('y', circle.node().getBBox().y + 55);
+  }
+}
