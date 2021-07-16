396795f077b8017779b6cb79499d4ad5b76ae3a7
Matej Miljko <matej@vanima.com>
2021-07-17 00:12:39 +0200

    fix slucajno uklonjen notification service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NotificationService} from "@app/_services/notification.service";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,6 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private notificationService: NotificationService) {
   }
 }
