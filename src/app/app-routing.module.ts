2e1f5e8f0440c52527fc7d83ac8cf3631cdda3a1
MeSlav <mislav.kordic@gmail.com>
2022-10-25 19:00:52 +0200

    Implement initial folder structure and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminViewComponent } from './views/admin-view/admin-view.component';
+import { UserViewComponent } from './views/user-view/user-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app',
+    component: UserViewComponent,
+  },
+  {
+    path: 'admin-app',
+    component: AdminViewComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventListComponent } from './events/components/event-list/event-list.component';
+import { EventItemComponent } from './events/components/event-item/event-item.component';
+import { EventViewComponent } from './events/components/event-view/event-view.component';
+import { AdminViewComponent } from './views/admin-view/admin-view.component';
+import { UserViewComponent } from './views/user-view/user-view.component';
+import { MapViewComponent } from './map/components/map-view/map-view.component';
+import { MapTableComponent } from './map/components/map-table/map-table.component';
+import { ReservationsViewComponent } from './reservations/components/reservations-view/reservations-view.component';
+import { ReservationsListComponent } from './reservations/components/reservations-list/reservations-list.component';
+import { ReservationItemComponent } from './reservations/components/reservation-item/reservation-item.component';
+import { UserCredentialsViewComponent } from './user-credentials/components/user-credentials-view/user-credentials-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventItemComponent,
+    EventViewComponent,
+    AdminViewComponent,
+    UserViewComponent,
+    MapViewComponent,
+    MapTableComponent,
+    ReservationsViewComponent,
+    ReservationsListComponent,
+    ReservationItemComponent,
+    UserCredentialsViewComponent
   ],
   imports: [
     BrowserModule,
